f61a82d85eb8c06d229773eb616df5cac3ca982e
Rishika Singh <97946128+rishikasingh08@users.noreply.github.com>
2022-01-23 13:36:56 +0530

    Add files via upload

--- Example.java
+++ Example.java
@@ -1,18 +1,6 @@
-//Method over loading.java
-         class Example{
-             static void m1(){
-                System.out.println("m1 no-param");
-            }
-        static void m1(int a){
-              System.out.println("m1 int-param");
-         }
-        static void m1(String s){
-             System.out.println("m1 String-param");
-}
-         public static void main(String[] args){
-             System.out.println("main");
-                      m1();
-                        m1("hk");
-                       
+package p1.p2;
+       class Example{
+		   public static void main(String[] args){
+			   System.out.println("hi");
+		   }
        }
-}
